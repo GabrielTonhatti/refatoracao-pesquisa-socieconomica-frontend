c96eedbb0e4cfc7a9dc952fdaf9f6c0572495dd4
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-08-19 09:14:23 -0300

    Atualiza material ui

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress } from "@material-ui/core";
+import { CircularProgress } from "@mui/material";
 import { ReactElement } from "react";
 import { DivLoading } from "./styles";
 
